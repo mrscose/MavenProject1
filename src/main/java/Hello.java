6120d7faa0736b6b640b295b08c177d7a9305bc5
cemre <mrsscose@gmail.com>
2021-12-22 19:26:13 +0000

    added more hello feature

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,9 +1,16 @@
+import java.sql.SQLOutput;
+
 public class Hello {
 
     public static void main(String[] args) {
 
         System.out.println("Hello");
         System.out.println("Developing hello feature");
+        System.out.println("Local Repo is already published to Github");
+        System.out.println("Doing more work");
+
+
+
     }
 
 }
